fe726ade62ecba44d522bdafb482ecc230d5b544
Tim van der Lippe <tvanderlippe@google.com>
2018-09-17 14:51:14 +0100

    Add documentation for public methods

--- src/state-messenger/StateMessenger.ts
+++ src/state-messenger/StateMessenger.ts
@@ -21,23 +21,44 @@ enum BroadCastType {
 const DEFAULT_TIMEOUT = 100;
 
 declare global {
+  /**
+   * Global interface to denote the types of state that are being transferred
+   * over a channel.
+   */
   interface StateMessengerChannelMap {}
 }
 
+/**
+ * Master node for state communication. Use {@link
+ * MasterStateMessenger#create()} to construct an instance. For TypeScript
+ * users, make sure to specify the type of state that you are communicating over
+ * the channel in the global {@link StateMessengerChannelMap} interface.
+ */
 export class MasterStateMessenger<S> extends BroadcastChannel {
   state: S;
 
-  constructor(channel: string, initialState: S) {
+  private constructor(channel: string, initialState: S) {
     super(channel);
 
     this.state = initialState;
   }
 
+  /**
+   * Construct a master that broadcasts state updates to the BroadcastChannel
+   * with name "channel".
+   *
+   * @param channel The channel name that is used to communicate.
+   */
   static create<C extends keyof StateMessengerChannelMap>(
       channel: C, initialState: StateMessengerChannelMap[C]) {
     return new MasterStateMessenger(channel, initialState);
   }
 
+  /**
+   * Start the master. It will announce to all connected clients that it can now
+   * start broadcasting state updates. Whenever a client is connected to the
+   * channel, it will automatically receive the current state from the master.
+   */
   start() {
     this.announceExistenceForClients();
     this.announceStateToClients();
@@ -48,6 +69,12 @@ export class MasterStateMessenger<S> extends BroadcastChannel {
     });
   }
 
+  /**
+   * Send a new state update to all connecting clients.
+   * @param state The new state object to publish. Make sure that the object can
+   *     be structurally cloned. See
+   *     https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm#Supported_types
+   */
   setState(state: S) {
     this.state = state;
     this.announceStateToClients();
@@ -66,24 +93,46 @@ export class MasterStateMessenger<S> extends BroadcastChannel {
 type StateCallback<S> = (state: S) => void;
 type MessageEventListener = (event: MessageEvent) => void;
 
+/**
+ * Client node for state communication. Use {@link
+ * ClientStateMessenger#create()} to construct an instance. For TypeScript
+ * users, make sure to specify the type of state that you are communicating over
+ * the channel in the global {@link StateMessengerChannelMap} interface.
+ */
 export class ClientStateMessenger<S> extends BroadcastChannel {
   callbackMap = new Map<StateCallback<S>, MessageEventListener>();
   timeout: number;
 
-  constructor(channel: string, timeout?: number) {
+  private constructor(channel: string, timeout?: number) {
     super(channel);
 
     this.timeout = timeout || DEFAULT_TIMEOUT;
   }
 
+  /**
+   * Construct a client that listens to the BroadcastChannel with name
+   * "channel".
+   *
+   * @param channel The channel name that is used to communicate.
+   */
   static create<C extends keyof StateMessengerChannelMap>(channel: C) {
     return new ClientStateMessenger<StateMessengerChannelMap[C]>(channel);
   }
 
+  /**
+   * Start the client while waiting on the master for state updates. If the
+   * master does not respond within {@link #timeout}ms, the returnging Promise
+   * is rejected.
+   */
   start() {
     return this.waitForMasterExistence();
   }
 
+  /**
+   * Listen for any changes broadcasted by the master. Make sure that the client
+   * is started by invoking {@link #start()} first.
+   * @param callback Callback function that supplies the new state.
+   */
   listen(callback: StateCallback<S>) {
     function eventCallback(event: MessageEvent) {
       if (event.data &&
@@ -96,6 +145,12 @@ export class ClientStateMessenger<S> extends BroadcastChannel {
     this.addEventListener('message', eventCallback);
   }
 
+  /**
+   * Unlisten to state changes.
+   *
+   * @param callback The original callback function that was passed into {@link
+   *     #listen(callback)}.
+   */
   unlisten(callback: StateCallback<S>) {
     const eventCallback = this.callbackMap.get(callback);
 
