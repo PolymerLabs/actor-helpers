fbfdfc0709673525e35143aa70b05dc956711e40
Paul Lewis <github@aerotwist.com>
2018-10-31 15:12:16 +0000

    Adds support for legacy codebases

--- demo/chess/rollup.config.js
+++ demo/chess/rollup.config.js
@@ -2,6 +2,7 @@ import typescript from "rollup-plugin-typescript2";
 import nodeResolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import loadz0r from "rollup-plugin-loadz0r";
+import legacy from "rollup-plugin-legacy";
 
 // Delete 'dist'
 require("rimraf").sync("dist");
@@ -24,6 +25,20 @@ export default {
       }
     }),
     nodeResolve(),
+    legacy({
+      "node_modules/garbo/js/garbochess.js": {
+        ResetGame: "ResetGame",
+        GetFen: "GetFen",
+        GetMoveFromString: "GetMoveFromString",
+        MakeMove: "MakeMove",
+        FormatMove: "FormatMove",
+        GenerateValidMoves: "GenerateValidMoves",
+        InitializeFromFen: "InitializeFromFen",
+        PVFromHash: "PVFromHash",
+        g_inCheck: "g_inCheck",
+        Search: "Search"
+      }
+    }),
     loadz0r(),
     terser()
   ],
